1afa4921f7d89571b08b9ab929135cf90533b5f9
heybluguy <heybluguy@yahoo.com>
2017-10-22 13:18:03 -0600

    attendee

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,7 +1,15 @@
 class Attendee
-  attr_reader :registration_date
+  attr_reader :registration_date, :first_name, :last_name, :email_address, :homephone, :street, :city, :state, :zipcode
   def initialize(info)
     @registration_date = info[:RegDate]
+    @first_name = info[:first_Name]
+    @last_name = info[:last_Name]
+    @email_address = info[:Email_Address]
+    @homephone = info[:HomePhone]
+    @street = info[:Street]
+    @city = info[:City]
+    @state = info[:State]
+    @zipcode = [:Zipcode]
   end
 
 end
--- test/attendee_test.rb
+++ test/attendee_test.rb
@@ -34,4 +34,20 @@ class AttendeeTest < Minitest::Test
 
     assert_equal "11/12/08 10:47", attendee.registration_date
   end
+
+  def test_attendee_has_a_name
+    attendee = Attendee.new({id:            "1",
+                             RegDate:       "11/12/08 10:47",
+                             first_Name:    "Allison",
+                             last_Name:     "Nguyen",
+                             Email_Address: "arannon@jumpstartlab.com",
+                             HomePhone:     "6154385000",
+                             Street:        "3155 19th St NW",
+                             City:          "Washington",
+                             State:         "DC",
+                             Zipcode:       "20010"}
+                           )
+
+    assert_equal "Allison", attendee.first_name
+  end
 end
