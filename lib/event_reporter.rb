181910b5720c5c869532a070566a37f3d92d114c
heybluguy <heybluguy@yahoo.com>
2017-10-23 23:26:16 -0600

    corrected test,reassigned contents and queue

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -3,32 +3,44 @@ require "./lib/cleaner"
 require "./lib/attendee"
 require "pry"
 
-
 class Reporter
   attr_reader :queue
+
   def initialize
      @cleaner = Cleaner.new
      @queue = []
   end
 
-  def populate_queue(csv_path)
-    contents = CSV.open csv_path, headers: true, header_converters: :symbol
-    contents.each do |row|
-      attendee = Attendee.new(row)
-      @queue << attendee
+  def load_file(csv_path = "./data/attendee_fixture.csv")
+    @contents = CSV.open csv_path, headers: true, header_converters: :symbol
+    @contents = @contents.map do |row|
+       Attendee.new(row)
     end
   end
 
   def find_first_names(name)
-    @queue.select do |attendee|
-      attendee if attendee.first_name == name
-    end
+    assign_queue
+      # @list.select { |attendee| @queue << attendee if attendee.first_name == name }
+      #dont erase this! this is a refactor that i made for you
+      # this select line is something that you can reuse for other queue commands later down the line
+      
+      @list.select do |attendee|
+      @queue << attendee if attendee.first_name == name
+      end
   end
 
   def queue_clear
     @queue = []
   end
 
+  def assign_queue
+    if @queue.empty?
+      @list = @contents
+    else
+      @list = @queue
+      queue_clear
+    end
+  end
 
 end
 
--- test/event_reporter_test.rb
+++ test/event_reporter_test.rb
@@ -3,40 +3,52 @@ require "minitest/pride"
 require "./lib/event_reporter"
 
 class ReporterTest < Minitest::Test
-  def test_queue_has_a_default_count
+  def test_queue_starts_empty
     reporter = Reporter.new
 
     assert_equal 0, reporter.queue.count
   end
 
-  def test_queue_has_a_count
-    reporter = Reporter.new
-    reporter.populate_queue("./data/attendee_fixture.csv")
-    assert_equal 10, reporter.queue.count
-  end
+  # def test_queue_has_a_count
+  #   reporter = Reporter.new
+  #   reporter.load_file("./data/attendee_fixture.csv")
+  #
+  #   assert_equal 10, reporter.queue.count
+  # end
 
   def test_it_can_find_a_firstname
     reporter = Reporter.new
-    reporter.populate_queue("./data/attendee_fixture.csv")
+    reporter.load_file("./data/attendee_fixture.csv")
 
-    attendees = reporter.find_first_names("Sarah")
-    assert_equal 2, attendees.count
+    reporter.find_first_names("Sarah")
+    assert_equal 2, reporter.queue.count
   end
 
-  def test_it_can_not_find_a_firstname
+  def test_queue_returns_empty_when_no_match_found
     reporter = Reporter.new
-    reporter.populate_queue("./data/attendee_fixture.csv")
+    reporter.load_file("./data/attendee_fixture.csv")
 
     assert_equal [], reporter.find_first_names("John")
   end
 
   def test_it_clears_queue
     reporter = Reporter.new
-    reporter.populate_queue("./data/attendee_fixture.csv")
+    reporter.load_file("./data/attendee_fixture.csv")
+    reporter.find_first_names("Sarah")
+    assert_equal 2, reporter.queue.count
+
     reporter.queue_clear
 
     assert_equal 0, reporter.queue.count
   end
 
-  
+  def test_it_has_default_csv_file
+    reporter = Reporter.new
+    reporter.load_file
+
+    assert reporter.queue.empty?
+  end
+
+
+
 end
