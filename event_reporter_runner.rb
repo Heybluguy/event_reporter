09f92b26ae011d44822439f3dfce1b8c1ed09c6f
heybluguy <heybluguy@yahoo.com>
2017-10-26 01:43:32 -0600

    things are working

--- event_reporter_runner.rb
+++ event_reporter_runner.rb
@@ -2,8 +2,9 @@ require './lib/event_reporter'
 
 event_reporter = EventReporter.new
 @input = nil
-
+puts "««« Event Reporter Initialized »»»"
 until @input == "quit" do
     @input = event_reporter.get_input
     event_reporter.evaluate(@input)
 end
+puts "««« Sequence Terminated »»»"
